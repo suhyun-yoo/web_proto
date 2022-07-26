ab3a07ae3338722da22acc454b69ea98f9db44be
Mingzhi <alswl_1110@naver.com>
2022-07-26 15:03:24 +0900

    부트스트랩 파일 업로드 - 타이탄

--- searchapp/models.py
+++ searchapp/models.py
@@ -2,9 +2,25 @@ from django.db import models
 from accounts.models import User
 
 
-class UploadImage(models.Model):
+# class UploadImage(models.Model):
+#     # title = models.CharField(max_length=255)
+#     uploadedFile = models.FileField(upload_to="UploadImg/%Y/%m/%d")
+#     create_date = models.DateTimeField(auto_now=True)
+#     request_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Request(models.Model):
     # title = models.CharField(max_length=255)
     uploadedFile = models.FileField(upload_to="UploadImg/%Y/%m/%d")
     create_date = models.DateTimeField(auto_now=True)
     request_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    done = models.BooleanField(default=False)
+
+
+class SearchedData(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.SET_NULL, null=True)
+    link = models.URLField(max_length=200)  #url이 얼마나 길어질지?
+    img = models.FileField(upload_to="SearchedImg/%Y/%m/%d")
+
+
+
 
--- searchapp/views.py
+++ searchapp/views.py
@@ -1,20 +1,23 @@
 from . import models
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import UploadImage
+# from .models import Request
 import os
 from django.conf import settings
 
 
 def uploadFile(request):
-    if request.method == "POST":
-        # Fetching the form data
-        uploadedFile = request.FILES["uploadedFile"]
-        user = request.user
-        # Saving the information in the database
-        uploadImg = models.UploadImage(
-            uploadedFile=uploadedFile,
-            request_user=user
-        )
-        uploadImg.save()
+    # if request.method == "POST":
+    #     # Fetching the form data
+    #     uploadedFile = request.FILES["uploadedFile"]
+    #     user = request.user
+    #     # Saving the information in the database
+    #     uploadImg = models.Request(
+    #         uploadedFile=uploadedFile,
+    #         request_user=user
+    #     )
+    #     uploadImg.save()
+    #     # search함수 부르기
+    #     # input -> uploadedFile
+    #     # Output -> SearchedData 타입, 저장하기.
 
     return render(request, "searchapp/upload-file.html")
